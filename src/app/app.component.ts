d3796dfee301c4481b6963237ce054e3b7c804e5
Michael Lancaster <michaell.llancaster@gmail.com>
2021-01-04 02:24:15 -0800

    simple angular v1 todo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-todo';
+  todos = ["buy bread"];
+
+  addNewTodo = (todo: string) => {
+    this.todos = [...this.todos, todo];
+  }
+
+  removeItem = (i: number) => {
+    const updatedTodos = [...this.todos];
+
+    updatedTodos.splice(i, 1);
+    this.todos = updatedTodos;
+  }
 }
